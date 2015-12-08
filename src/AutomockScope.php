35856d78bc232b49b4add0e82c5090c53db25da9
Kristopher Wilson <kristopherwilson@gmail.com>
2015-12-08 14:04:46 -0500

    Adding ability to provide mock defaults

--- src/AutomockScope.php
+++ src/AutomockScope.php
@@ -15,37 +15,48 @@ class AutomockScope extends Scope
     protected $prophet;
 
     /**
-     * @return Prophet
+     * @param string $class
+     * @param array $defaults
+     * @return object
      */
-    public function autoMock($class)
+    public function autoMock($class, array $defaults = [])
     {
         $reflectionClass = new ReflectionClass($class);
 
         if ($reflectionClass->hasMethod('__construct')) {
             return $reflectionClass->newInstanceArgs(
-                $this->collectArguments($reflectionClass->getMethod('__construct'))
+                $this->collectArguments($reflectionClass->getMethod('__construct'), $defaults)
             );
         }
 
         return $reflectionClass->newInstanceWithoutConstructor();
     }
 
-    private function collectArguments(ReflectionMethod $method)
+    /**
+     * @param ReflectionMethod $method
+     * @param array $defaults
+     * @return array
+     */
+    private function collectArguments(ReflectionMethod $method, array $defaults = [])
     {
         $parameterList = $method->getParameters();
         $mocks = [];
 
         foreach ($parameterList as $param) {
-            if (!$param->getClass()) {
-                throw new \RuntimeException(
-                    'You can only auto mock classes with class type-hinted constructor arguments'
-                );
+            if (array_key_exists($param->getName(), $defaults)) {
+                $mocks[] = $defaults[$param->getName()];
+            } else {
+                if (!$param->getClass()) {
+                    throw new \RuntimeException(
+                        'You can only auto mock classes with class type-hinted constructor arguments'
+                    );
+                }
+
+                $mock = (new Prophet())->prophesize($param->getClass()->getName());
+                $this->{$param->getName()} = $mock;
+
+                $mocks[] = $mock->reveal();
             }
-
-            $mock = (new Prophet())->prophesize($param->getClass()->getName());
-            $this->{$param->getName()} = $mock;
-
-            $mocks[] = $mock->reveal();
         }
 
         return $mocks;
